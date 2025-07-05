b04141df7d01f7bf5087a6eae95edf760ad89376
JesseW2158 <jessewang2158@gmail.com>
2025-07-05 01:24:36 -0400

    Finished Array class

--- Python/Math/ActivationFunctions.py
+++ Python/Math/ActivationFunctions.py
@@ -1,84 +1,86 @@
 import math
-from typing import Any
+from Util.DataStructures import *
+from typing import Union
 
 # ADD IN WHAT THE PURPOSE OF EACH ACTIVATION FUNCTION IS
 # TEST ALL ACTIVATION FUNCTIONS
 
 class Linear:
-    def __call__(self, x):
+    def __call__(self, x: Union[Array]):
         return x
     
-    def gradient(self, x):
-        return 1
+    # BASED OFF SHAPE OF X, CONSTRUCT NEW ARRAY WITH ONLY 1
+    def gradient(self, x: Union[Array]):
+        return x.shape()
     
-    def __str__(self):
+    def __str__(self) -> str:
         return "Linear"
     
 class BinaryStep:
-    def __call__(self, x):
+    def __call__(self, x: Union[Array]):
         return 1 if x >= 0 else 0
     
-    def gradient(self, x):
+    def gradient(self, x: Union[Array]):
         return 0
     
     def __str__(self) -> str:
         return "BinaryStep"
     
 class Sigmoid:
-    def __call__(self, x):
+    def __call__(self, x: Union[Array]):
         return 1 / 1 + (math.e ** -x)
     
-    def gradient(self, x):
+    def gradient(self, x: Union[Array]):
         return self(x) * (1 - self(x))
     
     def __str__(self) -> str:
         return "Sigmoid"
     
 class Tanh:
-    def __call__(self, x):
+    def __call__(self, x: Union[Array]):
         return (math.e ** x - math.e ** -x) / (math.e ** x + math.e ** -x)
 
-    def gradient(self, x):
+    def gradient(self, x: Union[Array]):
         return 1 - self(x) ** 2
     
     def __str__(self) -> str:
         return "Tanh"
     
 class SoftSign:
-    def __call__(self, x):
+    def __call__(self, x: Union[Array]):
         return x / (1 + abs(x))
     
-    def gradient(self, x):
+    def gradient(self, x: Union[Array]):
         return 1 / (1 + abs(x)) ** 2
     
     def __str__(self) -> str:
         return "SoftSign"
     
 class ReLU:
-    def __call__(self, x):
+    def __call__(self, x: Union[Array]):
         return max(0, x)
 
-    def gradient(self, x):
+    def gradient(self, x: Union[Array]):
         return 0 if x < 0 else 1
     
     def __str__(self) -> str:
         return "ReLU"
     
 class SoftPlus:
-    def __call__(self, x):
+    def __call__(self, x: Union[Array]):
         return math.log(1 + math.e ** x)
     
-    def gradient(self, x):
+    def gradient(self, x: Union[Array]):
         return 1 / (1 + math.e ** -x)
     
     def __str__(self) -> str:
         return "SoftPlus"
 
 class ELU:
-    def __call__(self, x, alpha=0.2) -> float:
+    def __call__(self, x: Union[Array], alpha=0.2) -> float:
         return alpha * (math.e ** x - 1) if x <= 0 else x
     
-    def gradient(self, x, alpha=0.2) -> float:
+    def gradient(self, x: Union[Array], alpha=0.2) -> float:
         return alpha * (math.e ** x) if x <= 0 else 1
     
     def __str__(self) -> str:
@@ -89,10 +91,10 @@ class SELU:
         self.scale = 1.0507009873554804934193349852946 
         self.alpha = 1.6732632423543772848170429916717
     
-    def __call__(self, x):
+    def __call__(self, x: Union[Array]):
         return self.scale * ELU(x)
     
-    def gradient(self, x):
+    def gradient(self, x: Union[Array]):
         return self.scale * ELU().gradient(x)
     
     def __str__(self) -> str:
@@ -102,10 +104,10 @@ class LeakyReLU:
     def __init__(self, alpha=0.01) -> None:
         self.alpha = alpha
     
-    def __call__(self, x):
+    def __call__(self, x: Union[Array]):
         return self.alpha * x if x <= 0 else x
     
-    def gradient(self, x):
+    def gradient(self, x: Union[Array]):
         return self.alpha if x < 0 else 1
     
     def __str__(self) -> str:
@@ -115,30 +117,30 @@ class PReLU:
     def __init__(self, alpha) -> None:
         self.alpha = alpha
         
-    def __call__(self, x):
+    def __call__(self, x: Union[Array]):
         return self.alpha * x if x < 0 else x
     
-    def gradient(self, x):
+    def gradient(self, x: Union[Array]):
         return self.alpha if x < 0 else 1
     
     def __str__(self) -> str:
         return "PReLU"
     
 class SiLU:
-    def __call__(self, x):
+    def __call__(self, x: Union[Array]):
         return x / (1 + math.e ** -x)
     
-    def gradient(self, x):
+    def gradient(self, x: Union[Array]):
         return (1 + math.e ** -x + x * math.e ** -x) / (1 + math.e ** -x) ** 2
     
     def __str__(self) -> str:
         return "SiLU"
 
 class Sinusoid:
-    def __call__(self, x):
+    def __call__(self, x: Union[Array]):
         return math.sin(x)
     
-    def gradient(self, x):
+    def gradient(self, x: Union[Array]):
         return math.cos(x)
     
     def __str__(self) -> str:
@@ -146,5 +148,5 @@ class Sinusoid:
     
 # TODO
 # class Softmax:
-#     def __call__(self, x):
+#     def __call__(self, x: Union[Array]):
         
---
+++ Python/Util/DataStructures.py
@@ -0,0 +1,80 @@
+from typing import Union
+
+# TODO implement __format_string before shape()
+
+class Array:
+    def __init__(self, array: Union[list, int]):
+        if not isinstance(array, list) and not isinstance(array, int):
+            raise Exception("Invalid array passed")
+        
+        if not self.__assert_equality(array):
+            raise Exception("Unequal array passed")
+        
+        if not self.__assert_int_array(array):
+            raise Exception("Array contains non-integers")
+        # check if all object types are the same in array
+        
+        self.array = array
+        
+    def __assert_int_array(self, array):
+        if isinstance(array[0], int) :
+            return all(type(element) == int for element in array)
+        
+        if not isinstance(array[0], list):
+            return False
+        
+        for element in array:
+            if not self.__assert_int_array(element):
+                return False
+            
+        return True
+        
+    def __assert_equality(self, array: Union[list, int]):
+        # checks if the array is a int, used for the first pass through
+        if isinstance(array, int):
+            return True
+        
+        # again for the first pass through, checks for a empty list
+        if len(array) == 0:
+            return True
+        
+        # keeps track of last list to ensure equal sizes between all lists
+        size = len(array[0]) if isinstance(array[0], list) else 0
+        
+        for element in array:
+            # checks if the inner array is a int array since if it wasnt a list, its a normal int array. Wont break since the previous recursive loop checked the size of the array already and made sure it was consistent with the rest of them
+            if not isinstance(element, list):
+                return True
+            
+            # checks if inner array is the same size as rest of the arrays
+            if len(element) != size:
+                return False
+            
+            if not self.__assert_equality(element):
+                return False
+        
+        return True
+
+    def __str__(self) -> str:
+        return f"{self.__format_array(self.array, 0)}"
+
+    def __format_array(self, array: list, depth: int):
+        return_str = ' ' * depth + '['
+        
+        if not isinstance(array[0], list):
+            return " " * depth + str(array)
+        
+        for element in array:
+            return_str += f"\n{self.__format_array(element, depth + 1)}"
+            
+        return(return_str + '\n' + ' ' * depth + ']')
+        
+    def shape(self) -> tuple:
+        return tuple(self.__shape(self.array))
+    
+    def __shape(self, array:list) -> int:
+        if isinstance(array[0], int):
+            return [len(array)]
+        
+        return [len(array)] + self.__shape(array[0])
+    
